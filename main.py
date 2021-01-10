262503c7528d37dd95a418a6f743f001b67c1439
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-09 21:39:12 -0500

    create layout and count down timer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,18 +14,58 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    # print(count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f'0{count % 60}'
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+# label
+timer = Label(text='Timer', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, 'normal'))
+timer.grid(column=1, row=0)
+
+
+# pomodoro
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) # allows us to layer images in window
 tomato_img = PhotoImage(file='assets/tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text='00:00', fill='#ffffff', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+timer_text = canvas.create_text(100, 130, text='00:00', fill='#ffffff', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=1, row=1)
+
+
+# button
+start = Button(text='start', highlightthickness=0, command=start_timer)
+start.config(padx=5, pady=5)
+start.grid(column=0, row=2)
+
+
+# label
+check_marks = Label(text='✅', bg=YELLOW, font=(FONT_NAME, 15, 'bold'))
+check_marks.grid(column=1, row=3)
+
+
+# button
+reset = Button(text='reset', highlightthickness=0)
+reset.config(padx=5, pady=5)
+reset.grid(column=2, row=2)
+
 
 window.mainloop()
